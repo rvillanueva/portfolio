720ccabb0f527f9a8b93c13b701c4a914a8c322c
Ryan Villanueva <ryanvill@gmail.com>
2021-12-11 17:20:06 -0800

    Fix email

--- src/pages/Contact/ContactPage.js
+++ src/pages/Contact/ContactPage.js
@@ -1,32 +1,37 @@
-import React from 'react';
-import './contact-page.css';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faEnvelopeOpenText} from '@fortawesome/free-solid-svg-icons';
-import {CSSTransition} from 'react-transition-group';
+import React from "react";
+import "./contact-page.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelopeOpenText } from "@fortawesome/free-solid-svg-icons";
+import { CSSTransition } from "react-transition-group";
 
 class ContactPage extends React.Component {
   constructor() {
     super();
     this.state = {
-      isLoaded: false
-    }
+      isLoaded: false,
+    };
   }
   componentDidMount() {
     this.setState({
-      isLoaded: true
+      isLoaded: true,
     });
   }
   render() {
     return (
       <div className="contact-page">
         <div className="contact__text-container">
-          <CSSTransition in={this.state.isLoaded} timeout={1000} classNames="envelope-animation">
+          <CSSTransition
+            in={this.state.isLoaded}
+            timeout={1000}
+            classNames="envelope-animation"
+          >
             <div className="contact-envelope">
               <FontAwesomeIcon icon={faEnvelopeOpenText} />
             </div>
           </CSSTransition>
           <div className="contact__text">
-            Interested in chatting? Shoot me an email at <a href="mailto:ryan@rvillanueva.co">ryan@rvillanueva.co</a>.
+            Interested in chatting? Shoot me an email at{" "}
+            <a href="mailto:ryan@rvillanueva.dev">ryan@rvillanueva.dev</a>.
           </div>
         </div>
       </div>
