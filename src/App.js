c1842ee9a9854dab556819db4292f5bcdf5bebb9
Ryan Villanueva <ryanvill@gmail.com>
2021-12-11 16:48:33 -0800

    Add url rewrite

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
-import {HomePage, ContactPage, AboutPage} from './pages';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import {Overlay, Drawer, Navbar, ScrollToTop} from './components';
-import {CSSTransition} from 'react-transition-group';
-import portfolioData from './data/portfolioData';
+import React from "react";
+import "./App.css";
+import { HomePage, ContactPage, AboutPage } from "./pages";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Overlay, Drawer, Navbar, ScrollToTop } from "./components";
+import { CSSTransition } from "react-transition-group";
+import portfolioData from "./data/portfolioData";
 
 class App extends React.Component {
   constructor() {
@@ -16,80 +16,114 @@ class App extends React.Component {
       loadPortfolio: false,
       overlayIsOpen: false,
       openProjectId: null,
-      drawerIsOpen: false
+      drawerIsOpen: false,
+    };
+  }
+  rewriteUrl() {
+    const url = new URL(window.location);
+    if (url.pathname.indexOf("index.html") > 0) {
+      window.location = "/";
     }
   }
   componentDidMount() {
+    this.rewriteUrl();
     const cutoff = 160;
-    window.addEventListener('scroll', () => {
-      if(window.scrollY > cutoff && !this.state.isScrolledDown) {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > cutoff && !this.state.isScrolledDown) {
         this.setState({
-          isScrolledDown: true
+          isScrolledDown: true,
         });
       } else if (window.scrollY <= cutoff && this.state.isScrolledDown) {
         this.setState({
-          isScrolledDown: false
+          isScrolledDown: false,
         });
       }
     });
-    setTimeout(() => this.setState({
-      loadBackground: true
-    }), 2000)
-    setTimeout(() => this.setState({
-      loadPortfolio: true
-    }), 750)
+    setTimeout(
+      () =>
+        this.setState({
+          loadBackground: true,
+        }),
+      2000
+    );
+    setTimeout(
+      () =>
+        this.setState({
+          loadPortfolio: true,
+        }),
+      750
+    );
     this.setState({
-      isLoaded: true
+      isLoaded: true,
     });
   }
-  openProjectById = projectId => {
+  openProjectById = (projectId) => {
     this.setState({
       overlayIsOpen: true,
-      openProjectId: projectId
-    })
-  }
+      openProjectId: projectId,
+    });
+  };
   closeOverlay = () => {
     this.setState({
       overlayIsOpen: false,
-      openProjectId: null
-    })
-  }
+      openProjectId: null,
+    });
+  };
   selectOpenProject = () => {
-    return portfolioData.items.filter(item => item._id === this.state.openProjectId)[0] || null;
-  }
-  toggleDrawer = bool => {
+    return (
+      portfolioData.items.filter(
+        (item) => item._id === this.state.openProjectId
+      )[0] || null
+    );
+  };
+  toggleDrawer = (bool) => {
     this.setState({
-      drawerIsOpen: typeof bool === 'boolean' ? bool : !this.state.drawerIsOpen
-    })
-  }
+      drawerIsOpen: typeof bool === "boolean" ? bool : !this.state.drawerIsOpen,
+    });
+  };
   render() {
     return (
       <Router className="app">
         <ScrollToTop />
-        <Navbar toggleDrawer={this.toggleDrawer} isScrolledDown={this.state.isScrolledDown}/>
+        <Navbar
+          toggleDrawer={this.toggleDrawer}
+          isScrolledDown={this.state.isScrolledDown}
+        />
         <CSSTransition
           mountOnEnter
           unmountOnExit
           in={this.state.overlayIsOpen}
           timeout={{
             enter: 200,
-            exit: 100
+            exit: 100,
           }}
-          classNames="fade-overlay">
-          <Overlay project={this.selectOpenProject()} close={this.closeOverlay}/>
+          classNames="fade-overlay"
+        >
+          <Overlay
+            project={this.selectOpenProject()}
+            close={this.closeOverlay}
+          />
         </CSSTransition>
-        <Drawer isOpen={this.state.drawerIsOpen} close={() => this.toggleDrawer(false)}/>
+        <Drawer
+          isOpen={this.state.drawerIsOpen}
+          close={() => this.toggleDrawer(false)}
+        />
         <div className="page-content">
           <Switch>
-              <Route path="/about" component={AboutPage} />
-              <Route path="/contact" component={ContactPage} />
-              <Route path="/" render={() => <HomePage
-                isLoaded={this.state.isLoaded}
-                isScrolledDown={this.state.isScrolledDown}
-                openProjectById={this.openProjectById}
-                loadBackground={this.state.loadBackground}
-                loadPortfolio={this.state.loadPortfolio}
-              />} />
+            <Route path="/about" component={AboutPage} />
+            <Route path="/contact" component={ContactPage} />
+            <Route
+              path="/"
+              render={() => (
+                <HomePage
+                  isLoaded={this.state.isLoaded}
+                  isScrolledDown={this.state.isScrolledDown}
+                  openProjectById={this.openProjectById}
+                  loadBackground={this.state.loadBackground}
+                  loadPortfolio={this.state.loadPortfolio}
+                />
+              )}
+            />
           </Switch>
         </div>
       </Router>
