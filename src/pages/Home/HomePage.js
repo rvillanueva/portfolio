0005453a9348212f4d73e7f695777c1e0a135da2
Ryan Villanueva <ryanvill@gmail.com>
2021-12-21 19:02:43 -0800

    Add profile photo

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,34 +1,42 @@
-import React from 'react';
-import Header from './components/Header/Header';
-import Portfolio from './components/Portfolio/Portfolio';
-import Background from './components/Background/Background';
-import portfolioData from '../../data/portfolioData';
-import './home-page.css';
-import {CSSTransition} from 'react-transition-group';
+import React from "react";
+import Header from "./components/Header/Header";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Background from "./components/Background/Background";
+import portfolioData from "../../data/portfolioData";
+import "./home-page.css";
+import { CSSTransition } from "react-transition-group";
 
-function HomePage({isLoaded, isScrolledDown, openProjectById, loadBackground, loadPortfolio}) {
+function HomePage({
+  isLoaded,
+  isScrolledDown,
+  openProjectById,
+  loadBackground,
+  loadPortfolio,
+}) {
   return (
     <div className="home-page">
-      { loadBackground ? <Background /> : null }
+      {loadBackground ? <Background /> : null}
       <CSSTransition
         mountOnEnter
         unmountOnExit
         in={isLoaded}
         timeout={2000}
-        classNames="fade-in-up">
+        classNames="fade-in-up"
+      >
         <Header />
       </CSSTransition>
-      { loadPortfolio ? null : <div className="home-page__header__spacer" />}
+      {loadPortfolio ? null : <div className="home-page__header__spacer" />}
       <CSSTransition
         mountOnEnter
         in={window.scrollY > 300 ? true : loadPortfolio}
         timeout={2000}
-        classNames="fade-in-portfolio">
-        <Portfolio openProjectById={openProjectById} items={portfolioData.items} />
+        classNames="fade-in-portfolio"
+      >
+        <Portfolio
+          openProjectById={openProjectById}
+          items={portfolioData.items}
+        />
       </CSSTransition>
-      <div className="self-splash" style={{
-          backgroundImage: `url('./images/self.jpg')`
-      }}></div>
     </div>
   );
 }
--- src/pages/Home/components/Header/Header.js
+++ src/pages/Home/components/Header/Header.js
@@ -1,22 +1,56 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
 
 class Header extends React.Component {
   getYearsText(y) {
-    const yearsText = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
+    const yearsText = [
+      "zero",
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+      "ten",
+      "eleven",
+      "twelve",
+      "thirteen",
+      "fourteen",
+      "fifteen",
+      "sixteen",
+      "seventeen",
+      "eighteen",
+      "nineteen",
+      "twenty",
+    ];
     return yearsText[y] || String(y);
   }
   render() {
     const yearsExperience = new Date().getFullYear() - 2014;
-    const yearsExperienceText = this.getYearsText(yearsExperience)
+    const yearsExperienceText = this.getYearsText(yearsExperience);
     return (
       <header className="home__header">
         <div className="home__header__text">
-          Ryan Villanueva is a full stack software engineer in New York City with over {yearsExperienceText} years of professional experience leading technical projects and teams. He graduated from Wharton and worked at IBM Research's innovation lab before moving on to venture-backed startups.
+          <div className="home__header__profile">
+            <img
+              src="./images/profile-photo.jpg"
+              className="home__header__profile__photo"
+              alt="Ryan Villanueva"
+            />
+          </div>
+          <div>
+            Ryan Villanueva is a full stack software engineer in New York City
+            with over {yearsExperienceText} years of professional experience
+            leading technical projects and teams. He graduated from Wharton and
+            worked at IBM Research's innovation lab before moving on to
+            venture-backed startups.
+          </div>
         </div>
       </header>
     );
-
   }
 }
 
