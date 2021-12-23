ed8bfcc3baa6b6097d084fe162e84a92e9b6aa6d
Ryan Villanueva <ryanvill@gmail.com>
2021-12-22 20:39:51 -0800

    Update Background.js

--- src/pages/Home/components/Background/Background.js
+++ src/pages/Home/components/Background/Background.js
@@ -10,21 +10,21 @@ class Particle {
     this.life = 0;
   }
   incrementLife() {
-    if (this.life < 1) this.life += 0.005;
+    if (this.life < 1) this.life += 0.0075;
   }
-  run(p5) {
+  run(p5, offsetY) {
     this.incrementLife();
     this.update();
-    this.draw(p5);
+    this.draw(p5, offsetY);
   }
   update() {
     this.position.add(this.velocity);
   }
-  draw = (p5) => {
-    const bottomColor = p5.color(245, 192, 118);
-    const topColor = p5.color(156, 118, 214);
+  draw = (p5, offsetY) => {
+    const bottomColor = p5.color(245, 192, 118, 120);
+    const topColor = p5.color(156, 118, 214, 200);
     const percent = Math.max(
-      Math.min((this.position.y + 100) / window.innerHeight, 1),
+      Math.min((this.position.y + 200) / window.innerHeight, 1),
       0
     );
     const baseColor = p5.lerpColor(topColor, bottomColor, percent);
@@ -33,7 +33,7 @@ class Particle {
     p5.noStroke();
     return p5.circle(
       this.position.x,
-      this.position.y,
+      this.position.y - offsetY * this.r * 0.5,
       this.r * window.devicePixelRatio
     );
   };
@@ -44,45 +44,56 @@ class Scene {
   addParticle(x, y, r, p5) {
     this.particles.push(new Particle(x, y, r, p5));
   }
-  clearParticles() {
+  clearParticles(height) {
     this.particles = this.particles.filter(({ position }) => {
       const { x, y } = position;
       if (x > window.innerWidth) return false;
       if (x < 0) return false;
-      if (y > window.innerHeight) return false;
+      if (y > height + 200) return false;
       if (y < 0) return false;
       return true;
     });
   }
-  run = (p5) => {
-    this.clearParticles();
+  run = (p5, offsetY, height) => {
+    this.clearParticles(height);
     for (let i = 0; i < this.particles.length; i++) {
-      this.particles[i].run(p5);
+      this.particles[i].run(p5, offsetY);
     }
   };
 }
 
 class Background extends React.Component {
+  offsetY = 0;
+  listener = null;
   scene = new Scene();
+  height = window.innerHeight + 300;
+  onScroll = this.handleOnScroll.bind(this);
+  componentDidMount() {
+    window.addEventListener("scroll", this.onScroll);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.onScroll);
+  }
+  handleOnScroll() {
+    this.offsetY = window.scrollY;
+  }
   setup = (p5, canvasParentRef) => {
-    p5.createCanvas(window.innerWidth, window.innerHeight).parent(
-      canvasParentRef
-    );
+    p5.createCanvas(window.innerWidth, this.height).parent(canvasParentRef);
   };
   draw = (p5) => {
     p5.clear();
-    const multiplier = (window.innerWidth * window.innerHeight) / 20000;
+    const multiplier = (window.innerWidth * this.height) / 20000;
     const needsMoreParticles =
       this.scene.particles.length < Math.round(multiplier);
     if (needsMoreParticles) {
       this.scene.addParticle(
         Math.round(Math.random() * window.innerWidth),
-        Math.round(Math.random() * window.innerHeight),
+        Math.round(Math.random() * this.height),
         Math.random() * 3,
         p5
       );
     }
-    this.scene.run(p5);
+    this.scene.run(p5, this.offsetY, this.height);
   };
   render() {
     return (
