79279e88aaa591df5379e1df4d8bac4b4e57d81a
Ryan Villanueva <ryanvill@gmail.com>
2023-12-05 19:35:43 +0000

    Update build

---
+++ src/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: process.env.FRONTEND_PORT ? Number(process.env.FRONTEND_PORT) : 5000,
+  },
+  build: {
+    outDir: "../build",
+  },
+});
