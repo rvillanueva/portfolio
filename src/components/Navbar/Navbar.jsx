ae874b5cc404b9df1ae575c5539fc45543580d7e
Ryan Villanueva <ryanvill@gmail.com>
2023-12-06 18:19:43 +0000

    Replace icons with react-icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,42 @@
-import React from 'react';
-import './navbar.css';
-import {CSSTransition} from 'react-transition-group';
-import {Link} from 'react-router-dom';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faBars} from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import "./navbar.css";
+import { CSSTransition } from "react-transition-group";
+import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa6";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
-function Navbar({hideLogo, toggleDrawer, isScrolledDown}) {
+function Navbar({ hideLogo, toggleDrawer, isScrolledDown }) {
   return (
-    <div className={`navbar${isScrolledDown ? ' navbar--scrolled' : ''}`}>
+    <div className={`navbar${isScrolledDown ? " navbar--scrolled" : ""}`}>
       <div className="navbar__left">
         <CSSTransition
           mountOnEnter
           in={!hideLogo}
           timeout={750}
-          classNames="fade-in-logo">
-          <Link to="/" className="navbar__logo">Ryan Villanueva</Link>
+          classNames="fade-in-logo"
+        >
+          <Link to="/" className="navbar__logo">
+            Ryan Villanueva
+          </Link>
         </CSSTransition>
       </div>
       <div className="navbar__right">
         <div className="navbar__links">
-          <Link className="navbar__link" to="/about">About</Link>
-          <a className="navbar__link" href="https://medium.com/@ryanjvillanueva">Blog</a>
-          <Link className="navbar__link" to="/contact">Contact</Link>
+          <Link className="navbar__link" to="/about">
+            About
+          </Link>
+          <a
+            className="navbar__link"
+            href="https://medium.com/@ryanjvillanueva"
+          >
+            Blog
+          </a>
+          <Link className="navbar__link" to="/contact">
+            Contact
+          </Link>
         </div>
         <div className="navbar__drawer-toggle" onClick={toggleDrawer}>
-          <FontAwesomeIcon icon={faBars} />
+          <FaBars />
         </div>
       </div>
     </div>
